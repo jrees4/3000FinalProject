1b636d9d7140cefb62b75139c682d203b11880ae
jrees4 <44977311+jrees4@users.noreply.github.com>
2021-02-08 18:03:50 +0000

    Gradient Quad + Inspector
    Modified colour script to change each pixel based on their place within the resolution ( gradient). and changed default filter (bilinear) to point based.
    
    Created a script for a custom inspector. It draws the default inspector and checks for changes. If a change happened, it calls the FillTexture from our texture creating script.

---
+++ Noise/Noise Project/Assets/Editor/TextureCreatorInspector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(TextureCreator))]
+public class TextureCreatorInspector : Editor
+{
+    private TextureCreator creator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable(){
+        creator = target as TextureCreator;
+        Undo.undoRedoPerformed += RefreshCreator;
+    }
+
+    private void OnDisable(){
+        Undo.undoRedoPerformed += RefreshCreator;
+    }
+
+    private void RefreshCreator() {
+        if(Application.isPlaying) {
+            creator.FillTexture();
+        }
+    }
+
+    public override void OnInspectorGUI () {
+        EditorGUI.BeginChangeCheck();
+        DrawDefaultInspector();
+        if (EditorGUI.EndChangeCheck() ){ //&& Application.isPlaying
+            RefreshCreator();
+            //(target as TextureCreator).FillTexture();
+        }
+    }
+}
--- Noise/Noise Project/Assets/Scripts/TextureCreator.cs
+++ Noise/Noise Project/Assets/Scripts/TextureCreator.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class TextureCreator : MonoBehaviour
 {
-    
+    [Range (2, 512)]
     public int resolution = 256;  //size. 16 * 16
     private Texture2D texture;
     
     // AWAKE is called when the instance is being loaded. (before app starts)
     void Awake(){
+        
+    }
+
+    // On Enable called when the component is activated.
+    void OnEnable() {
         texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, true);
         texture.name = "Procedural Texture";
+        texture.wrapMode = TextureWrapMode.Clamp; // Stops edges being fucky
+        texture.filterMode = FilterMode.Point; //Point filtering instead of defeault (bilinear)
         GetComponent<MeshRenderer>().material.mainTexture = texture;
         FillTexture();
     }
@@ -28,10 +35,14 @@ public class TextureCreator : MonoBehaviour
         
     }
 
-    private void FillTexture() {
+    public void FillTexture() {
+        if (texture.width != resolution){
+            texture.Resize(resolution, resolution);
+        }
+        float stepSize = 1f / resolution;
         for (int y = 0; y < resolution; y++){
             for (int x = 0; x < resolution; x++){
-                texture.SetPixel(x , y , Color.red);
+                texture.SetPixel(x , y , new Color((x + 0.5f) * stepSize, (y + 0.5f) * stepSize, 0f));
             }
         }
         texture.Apply();
