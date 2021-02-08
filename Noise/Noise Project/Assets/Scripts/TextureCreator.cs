7a5bbfc26344c3ade9ee552dc91baaa725ca8d3f
jrees4 <44977311+jrees4@users.noreply.github.com>
2021-02-08 16:40:37 +0000

    Created Noise Project
    A basic change colour from null to red using nested loops within unity. A texture generated by code.
    
    The reason for this project is to create/test customisable noise for the Main Project.

---
+++ Noise/Noise Project/Assets/Scripts/TextureCreator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureCreator : MonoBehaviour
+{
+    
+    public int resolution = 256;  //size. 16 * 16
+    private Texture2D texture;
+    
+    // AWAKE is called when the instance is being loaded. (before app starts)
+    void Awake(){
+        texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, true);
+        texture.name = "Procedural Texture";
+        GetComponent<MeshRenderer>().material.mainTexture = texture;
+        FillTexture();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FillTexture() {
+        for (int y = 0; y < resolution; y++){
+            for (int x = 0; x < resolution; x++){
+                texture.SetPixel(x , y , Color.red);
+            }
+        }
+        texture.Apply();
+    }
+}
