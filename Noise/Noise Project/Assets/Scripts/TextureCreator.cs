2b01e4e630ebe28c29e9222f94a79a5d97c1810f
jrees4 <44977311+jrees4@users.noreply.github.com>
2021-02-10 15:57:07 +0000

    Used interpolation for points between.
    Made it so you can transform and the shape updates aswell.
    
    Messed around with different filtermodes.

--- Noise/Noise Project/Assets/Scripts/TextureCreator.cs
+++ Noise/Noise Project/Assets/Scripts/TextureCreator.cs
@@ -15,11 +15,14 @@ public class TextureCreator : MonoBehaviour
 
     // On Enable called when the component is activated.
     void OnEnable() {
+        if (texture == null) {
         texture = new Texture2D(resolution, resolution, TextureFormat.RGB24, true);
         texture.name = "Procedural Texture";
         texture.wrapMode = TextureWrapMode.Clamp; // Stops edges being fucky
-        texture.filterMode = FilterMode.Point; //Point filtering instead of defeault (bilinear)
+        texture.filterMode = FilterMode.Trilinear; //Point filtering instead of defeault (bilinear) OR Point OR Trilinear
+        texture.anisoLevel = 9;
         GetComponent<MeshRenderer>().material.mainTexture = texture;
+        }
         FillTexture();
     }
 
@@ -32,17 +35,36 @@ public class TextureCreator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //make sure its up to date while transforming,  this allows to changing the shape during play.
+		if (transform.hasChanged) {
+			transform.hasChanged = false;
+			FillTexture();
+		}
+	
     }
 
     public void FillTexture() {
         if (texture.width != resolution){
             texture.Resize(resolution, resolution);
         }
+
+        //Four corners.  pointX,Y
+        Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f,-0.5f));
+		Vector3 point10 = transform.TransformPoint(new Vector3( 0.5f,-0.5f));
+		Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
+		Vector3 point11 = transform.TransformPoint(new Vector3( 0.5f, 0.5f));
+
         float stepSize = 1f / resolution;
         for (int y = 0; y < resolution; y++){
-            for (int x = 0; x < resolution; x++){
-                texture.SetPixel(x , y , new Color((x + 0.5f) * stepSize, (y + 0.5f) * stepSize, 0f));
+            // Interpolate (insert) points between points.  The .lerp function does this.
+            Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) * stepSize); //left
+            //Debug.Log("Rotation: " + y + " Point 0: -- " + point0);
+			Vector3 point1 = Vector3.Lerp(point10, point11, (y + 0.5f) * stepSize); //right
+            //Debug.Log("Rotation: " + y + "Point 1: -- " + point1);
+            for (int x = 0; x < resolution; x++){ //for
+                Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize); // point between left and right.
+                //Debug.Log("Rotation: " + y + "version:  " + x + "Point : -- " + point);
+                texture.SetPixel(x , y , new Color(point.x, point.y, point.z));  //OLD: ((x + 0.5f) * stepSize % 0.1f, (y + 0.5f) * stepSize % 0.1f, 0f) * 10f )
             }
         }
         texture.Apply();
